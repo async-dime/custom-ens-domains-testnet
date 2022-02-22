fffa32e4c8f287aa133ad3f59076959c414ae439
Bramandia Gilanglaksana Adam <gilangbram@gmail.com>
2022-02-23 00:34:59 +0700

    :zap: update

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,7 +2,6 @@ require('@nomiclabs/hardhat-waffle');
 require('dotenv').config({ path: '.env' });
 
 const ALCHEMY_API_KEY_URL = process.env.ALCHEMY_API_KEY_URL;
-const PROD_ALCHEMY_API_KEY_URL = process.env.PROD_ALCHEMY_API_KEY_URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY_MAIN;
 
 module.exports = {
@@ -11,12 +10,6 @@ module.exports = {
     // testnet
     mumbai: {
       url: ALCHEMY_API_KEY_URL,
-      accounts: [PRIVATE_KEY],
-    },
-    // mainnet
-    mainnet: {
-      chainId: 1,
-      url: PROD_ALCHEMY_API_KEY_URL,
       accounts: [PRIVATE_KEY],
     },
   },
